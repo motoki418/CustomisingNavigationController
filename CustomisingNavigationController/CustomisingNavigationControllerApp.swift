a4fcf10a29d64a0563e78f9987eca78ac05b088f
motoki418 <snkrs418@gmail.com>
2022-02-14 22:05:16 +0900

    FirstCommit

---
+++ CustomisingNavigationController/CustomisingNavigationControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomisingNavigationControllerApp.swift
+//  CustomisingNavigationController
+//
+//  Created by nakamura motoki on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct CustomisingNavigationControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
